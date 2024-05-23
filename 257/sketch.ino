be4d60f48a7b5ebc9ff3d6fdf78c955317226981
arnavshroff <74356348+arnavshroff@users.noreply.github.com>
2024-05-23 20:37:16 +0530

    .

--- 257/sketch.ino
+++ 257/sketch.ino
@@ -16,7 +16,7 @@ ezButton button3(14);
 ezButton button4(27);
 ezButton button5(26);
 
-int vote1 = 0, vote2 = 0, vote3 = 0, vote4 = 0;
+int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
 
 int flag = 0;
 
@@ -41,11 +41,13 @@ void setup()
   oled.setTextColor(WHITE);    // set text color
 
   oled.setCursor(2, 5); 
-  oled.println("Start");       // set position to display
-  oled.setCursor(2, 21); 
-  oled.println("voting..");   
+  oled.println("Press");       // set position to display
+  oled.setCursor(2, 25); 
+  oled.println("floor");
+  oled.setCursor(2, 45); 
+  oled.println("no.");
   oled.display();             // display on OLED
-  delay(2000);
+  delay(1000);
 }
 
 void loop() {
@@ -56,24 +58,6 @@ void loop() {
   button4.loop();
   button5.loop();
 
-  if (flag == 0) {
-   // set text
-    oled.clearDisplay(); // clear display
-    oled.setTextSize(2);         // set text size
-    oled.setTextColor(WHITE);    // set text color
-
-    oled.setCursor(2, 0);       // set position to display
-    oled.println("A - Green ");
-    oled.setCursor(2, 16);       // set position to display
-    oled.println("B - Yellow");
-    oled.setCursor(2, 32);       // set position to display
-    oled.println("C - Red");     // display on OLED
-    oled.setCursor(2, 48);       // set position to display
-    oled.println("D - Orange");
-    oled.display();
-    flag=1;
-    
-  }
-
-  delay(10);
+  delay(1000);
+  oled.clearDisplay();
 }
